08b4c1ca7bce1c42b6593ad8535310efb8fb9dba
Danil <fs159@mail.ru>
2020-09-24 21:29:20 +0300

    Added new tests

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -1,7 +1,7 @@
 import unittest
 import random
 import datetime
-from simple_calculator import Calculator
+from src.simple_calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
@@ -39,6 +39,27 @@ class TestCalculator(unittest.TestCase):
     def test_root(self):
         calc_value = self.calculator.value
         self.assertEqual(self.calculator.root(2).value, calc_value ** (1/2))
+
+    def test_kek1(self):
+        calc_value = self.calculator.value
+        self.assertNotEqual(self.calculator.root(25).divide(3.14, 16).add(1, 41, 4).multiply(2.999999).value,
+                            calc_value ** (1/2))
+
+    def test_kek2(self):
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.root(2).divide(3.14, 15).add(1, 41, 4).multiply(2.71828).value,
+                         ((calc_value ** (1/2)) / 3.14 / 15 + (1 + 41 + 4)) * 2.71828)
+
+    def test_kek3(self):
+        calc_value = self.calculator.value
+        self.assertEqual(self.calculator.power(99).subtract(1234.9876).root(985).multiply(33).value,
+                         (((calc_value ** 99) - 1234.9876) ** (1 / 985)) * 33)
+
+    def test_kek4(self):
+        calc_value = self.calculator.value
+        self.assertAlmostEqual(self.calculator.multiply(11, 3, 5, 1).add(123.3456789, 9999.99999).power(2)
+                               .root(123).value,
+                               round(((calc_value * 11 * 3 * 5 * 1 + 123.3456789 + 9999.99999) ** 2) ** (1 / 123), 7))
 
 
 if __name__ == '__main__':
